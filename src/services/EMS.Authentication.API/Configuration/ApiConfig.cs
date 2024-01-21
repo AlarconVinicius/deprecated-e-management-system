60faabc8d82091c0e9b92693e2500d8fe826abc3
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-21 13:06:48 -0300

    feat: Add Identity Configs

---
+++ src/services/EMS.Authentication.API/Configuration/ApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using EMS.WebAPI.Core.Authentication;
+
+namespace EMS.Authentication.API.Configuration;
+
+public static class ApiConfig
+{
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthConfiguration();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        return app;
+    }
+}
---
+++ src/services/EMS.Authentication.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,27 @@
+﻿using EMS.WebAPI.Core.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+
+namespace EMS.Authentication.API.Configuration;
+
+public static class IdentityConfig
+{
+    public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddDefaultIdentity<IdentityUser>()
+            .AddRoles<IdentityRole>()
+            .AddErrorDescriber<IdentityMensagensPortugues>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddJwtConfiguration(configuration);
+
+        return services;
+    }
+}
