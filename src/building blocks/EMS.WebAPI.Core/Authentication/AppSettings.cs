7fa360ff98023256b4f762d9a8d81646a6ef704f
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-21 12:58:25 -0300

    feat: Add JWT Configs

---
+++ src/building blocks/EMS.WebAPI.Core/Authentication/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace EMS.WebAPI.Core.Authentication;
+
+public class AppSettings
+{
+    public string? Secret { get; set; }
+    public int ExpiracaoHoras { get; set; }
+    public string? Emissor { get; set; }
+    public string? ValidoEm { get; set; }
+}
