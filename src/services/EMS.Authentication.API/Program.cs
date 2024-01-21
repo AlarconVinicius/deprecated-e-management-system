d9bf38f2d9c934ea8a2289e3050f54549a983129
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-21 13:07:32 -0300

    feat: Add Authentication Project configs

---
+++ src/services/EMS.Authentication.API/Program.cs
@@ -0,0 +1,22 @@
+using EMS.Authentication.API.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+builder.Services.AddIdentityConfiguration(builder.Configuration);
+builder.Services.AddApiConfiguration();
+builder.Services.AddSwaggerConfiguration();
+builder.Services.AddMessageBusConfiguration(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseSwaggerConfiguration();
+
+app.UseApiConfiguration(app.Environment);
+
+
+app.Run();
