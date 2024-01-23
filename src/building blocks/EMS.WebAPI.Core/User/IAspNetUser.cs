11754b5e370a7bf4122c6267fd2370e0cf7ef86c
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-23 13:20:01 -0300

    refact: Update method names

--- src/building blocks/EMS.WebAPI.Core/User/IAspNetUser.cs
+++ src/building blocks/EMS.WebAPI.Core/User/IAspNetUser.cs
@@ -6,11 +6,11 @@ namespace EMS.WebAPI.Core.User;
 public interface IAspNetUser
 {
     string Name { get; }
-    Guid ObterUserId();
-    string ObterUserEmail();
-    string ObterUserToken();
-    bool EstaAutenticado();
-    bool PossuiRole(string role);
-    IEnumerable<Claim> ObterClaims();
-    HttpContext ObterHttpContext();
+    Guid GetUserId();
+    string GetUserEmail();
+    string GetUserToken();
+    bool IsAuthenticated();
+    bool HasRole(string role);
+    IEnumerable<Claim> GetClaims();
+    HttpContext GetHttpContext();
 }
