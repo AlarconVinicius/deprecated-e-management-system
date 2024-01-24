564a8444c3c95bcbfb3ac72b2e11f89678d2b941
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-24 13:38:05 -0300

    feat: Add Subscription API

---
+++ src/services/EMS.Subscription.API/Configuration/ApiConfig.cs
@@ -0,0 +1,62 @@
+﻿using EMS.Subscription.API.Data;
+using EMS.WebAPI.Core.Authentication;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Subscription.API.Configuration;
+
+public static class ApiConfig
+{
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<SubscriptionContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("Total",
+                builder =>
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseCors("Total");
+
+        app.UseAuthConfiguration();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        return app;
+    }
+
+    public static void CheckAndApplyDatabaseMigrations(this IApplicationBuilder app, IServiceProvider services)
+    {
+        using var scope = services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<SubscriptionContext>();
+        if (dbContext.Database.GetPendingMigrations().Any())
+        {
+            dbContext.Database.Migrate();
+        }
+    }
+}
