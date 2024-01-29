f3eb4c7de158dd1f4b68634d475cd6a706afb3fe
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-29 20:06:27 -0300

    feat: Add product page

---
+++ src/web/EMS.WebApp.MVC/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.WebApp.MVC.Controllers;
+
+[Authorize]
+public class ProductController : MainController
+{
+    [Route("produtos")]
+    public IActionResult Products()
+    {
+        return View();
+    }
+}
