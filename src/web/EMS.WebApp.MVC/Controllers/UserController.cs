4d62ce38d36f82774c256e037d00ece9ca7129c8
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-29 20:06:15 -0300

    feat: Add users pages

---
+++ src/web/EMS.WebApp.MVC/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.WebApp.MVC.Controllers;
+
+[Authorize]
+public class UserController : MainController
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [Route("perfil")]
+    public IActionResult Profile()
+    {
+        return View();
+    }
+
+    [Route("assinantes")]
+    public IActionResult Subscribers()
+    {
+        return View();
+    }
+
+    [Route("colaboradores")]
+    public IActionResult Employees()
+    {
+        return View();
+    }
+
+    [Route("clientes")]
+    public IActionResult Clients()
+    {
+        return View();
+    }
+}
