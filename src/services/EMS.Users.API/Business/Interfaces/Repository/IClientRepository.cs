c9252f414d1df2835d901d82bce18a2a011175f1
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-31 11:32:57 -0300

    feat: Implement repository for each type of user

---
+++ src/services/EMS.Users.API/Business/Interfaces/Repository/IClientRepository.cs
@@ -0,0 +1,14 @@
+﻿using EMS.Core.Data;
+using EMS.Users.API.Models;
+
+namespace EMS.Users.API.Business.Interfaces.Repository;
+
+public interface IClientRepository : IRepository<Client>
+{
+    void AddClient(Client client);
+    void UpdateClient(Client client);
+    void DeleteClient(Client client);
+    Task<IEnumerable<Client>> GetAllClients();
+    Task<Client> GetById(Guid id);
+    Task<Client> GetByCpf(string cpf);
+}
---
+++ src/services/EMS.Users.API/Data/Repository/ClientRepository.cs
@@ -0,0 +1,53 @@
+﻿using EMS.Core.Data;
+using EMS.Users.API.Business.Interfaces.Repository;
+using EMS.Users.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Users.API.Data.Repository;
+
+public class ClientRepository : IClientRepository
+{
+    private readonly UsersContext _context;
+
+    public ClientRepository(UsersContext context)
+    {
+        _context = context;
+    }
+
+    public IUnitOfWork UnitOfWork => _context;
+
+    public async Task<IEnumerable<Client>> GetAllClients()
+    {
+        return await _context.Clients.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Client> GetById(Guid id)
+    {
+        return await _context.Clients.FirstOrDefaultAsync(c => c.Id == id) ?? null!;
+    }
+
+    public async Task<Client> GetByCpf(string cpf)
+    {
+        return await _context.Clients.FirstOrDefaultAsync(c => c.Cpf.Number == cpf) ?? null!;
+    }
+
+    public void AddClient(Client client)
+    {
+        _context.Clients.Add(client);
+    }
+
+    public void UpdateClient(Client client)
+    {
+        _context.Clients.Update(client);
+    }
+
+    public void DeleteClient(Client client)
+    {
+        _context.Clients.Remove(client);
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
