e66ebfac555cdc0b261af80c6da0fbe2f8727508
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-01-31 11:44:50 -0300

    feat: Add UsersController

---
+++ src/services/EMS.Users.API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using EMS.Users.API.Business.Interfaces.Service;
+using EMS.Users.API.Models.Dtos;
+using EMS.WebAPI.Core.Controllers;
+using EMS.WebAPI.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Users.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : MainController
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(INotifier notifier, IUserService userService) : base(notifier)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(UserAddDto user)
+        {
+            await _userService.AddUser(user);
+            return CustomResponse(user);
+        }
+    }
+}
