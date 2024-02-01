584127d186efd334d7f188e786d4a6c0730798b5
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-02-01 11:18:42 -0300

    feat: Add client CRUD methods

--- src/web/EMS.WebApp.MVC/Services/ClientService.cs
+++ src/web/EMS.WebApp.MVC/Services/ClientService.cs
@@ -1,6 +1,7 @@
 ﻿using EMS.WebApp.MVC.Extensions;
 using EMS.WebApp.MVC.Models;
 using Microsoft.Extensions.Options;
+using System.Net;
 
 namespace EMS.WebApp.MVC.Services;
 
@@ -18,7 +19,7 @@ public class ClientService : Service, IClientService
 
     public async Task<ClientViewModel> GetByCpf(string cpf, Guid userId)
     {
-        var response = await _httpClient.GetAsync($"/api/users/clients/cpf/{cpf}?userId={userId}");
+        var response = await _httpClient.GetAsync($"/api/clients/cpf/{cpf}?userId={userId}");
 
         HandleErrorResponse(response);
 
@@ -27,10 +28,70 @@ public class ClientService : Service, IClientService
 
     public async Task<IEnumerable<ClientViewModel>> GetAll(Guid userId)
     {
-        var response = await _httpClient.GetAsync($"/api/users/clients?userId={userId}");
+        var response = await _httpClient.GetAsync($"/api/clients?userId={userId}");
 
         HandleErrorResponse(response);
 
         return await DeserializeResponseObject<IEnumerable<ClientViewModel>>(response);
+    }
+
+    public async Task<ClientResponse> AddClient(ClientViewModel client)
+    {
+        var clientContent = GetContent(client);
+
+        var response = await _httpClient.PostAsync("/api/clients", clientContent);
+
+        if (!HandleErrorResponse(response))
+        {
+            return new ClientResponse
+            {
+                ResponseResult = await DeserializeResponseObject<ResponseResult>(response)
+            };
+        }
+
+        if (response.StatusCode == HttpStatusCode.NoContent)
+        {
+            return new ClientResponse();
+        }
+
+        return await DeserializeResponseObject<ClientResponse>(response);
+    }
+
+    public async Task<ClientResponse> UpdateClient(ClientViewModel client)
+    {
+        var clientContent = GetContent(client);
+
+        var response = await _httpClient.PutAsync("/api/clients", clientContent);
+
+        if (!HandleErrorResponse(response))
+        {
+            return new ClientResponse
+            {
+                ResponseResult = await DeserializeResponseObject<ResponseResult>(response)
+            };
+        }
+
+        if (response.StatusCode == HttpStatusCode.NoContent)
+        {
+            return new ClientResponse();
+        }
+
+        return await DeserializeResponseObject<ClientResponse>(response);
+    }
+
+    public async Task<ClientResponse> DeleteClient(Guid id, Guid userId)
+    {
+        var response = await _httpClient.DeleteAsync($"/api/clients/{id}?userId={userId}");
+
+        if (!HandleErrorResponse(response))
+        {
+            return new ClientResponse
+            {
+                ResponseResult = await DeserializeResponseObject<ResponseResult>(response)
+            };
+        }
+
+
+        return new ClientResponse();
     }
 }
--- src/web/EMS.WebApp.MVC/Services/IClientService.cs
+++ src/web/EMS.WebApp.MVC/Services/IClientService.cs
@@ -6,4 +6,7 @@ public interface IClientService
 {
     Task<IEnumerable<ClientViewModel>> GetAll(Guid userId);
     Task<ClientViewModel> GetByCpf(string cpf, Guid userId);
+    Task<ClientResponse> AddClient(ClientViewModel client);
+    Task<ClientResponse> UpdateClient(ClientViewModel client);
+    Task<ClientResponse> DeleteClient(Guid id, Guid userId);
 }
